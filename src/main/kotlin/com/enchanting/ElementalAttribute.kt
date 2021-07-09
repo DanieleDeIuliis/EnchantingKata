f284aa8583c0be34c610f17dcd1a7c835ffc33e3
Daniele De Iuliis <daniele.de.iuliis@it.clara.net>
2021-07-09 10:58:06 +0200

    enchanting a weapon removes the old enchantment

--- src/main/kotlin/com/enchanting/ElementalAttribute.kt
+++ src/main/kotlin/com/enchanting/ElementalAttribute.kt
@@ -1,5 +1,6 @@
 package com.enchanting
 
 enum class ElementalAttribute(val prefix: String, val bonus: String) {
-    FIRE("Inferno", "+5 fire damage")
+    FIRE("Inferno", "+5 fire damage"),
+    ICE("Icy", "+5 ice damage")
 }
--- src/test/kotlin/com/enchanting/MagicBookTest.kt
+++ src/test/kotlin/com/enchanting/MagicBookTest.kt
@@ -1,5 +1,6 @@
 package com.enchanting
 
+import com.enchanting.ElementalAttribute.*
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -12,7 +13,7 @@ class MagicBookTest {
 
         magicBook.enchant(weapon)
 
-        assertThat(weapon.elementalAttribute).isEqualTo(ElementalAttribute.FIRE)
+        assertThat(weapon.elementalAttribute).isEqualTo(FIRE)
     }
 
     @Test
@@ -23,6 +24,16 @@ class MagicBookTest {
         magicBook.enchant(weapon)
 
         assertThat(weapon.name).isEqualTo("Inferno Staff")
+    }
+
+    @Test
+    fun `enchanting a weapon removes the old enchantment`() {
+        val weapon = Weapon("Staff", ICE)
+        val magicBook = MagicBook()
+
+        magicBook.enchant(weapon)
+
+        assertThat(weapon.elementalAttribute).isEqualTo(FIRE)
     }
 }
 
